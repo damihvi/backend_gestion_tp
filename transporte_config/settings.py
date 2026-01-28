922b6402a70332af48c96c1bc4a3a215aa8d9290
damihvi <dami-anherrera@hotmail.com>
2026-01-28 14:21:32 -0500

    actualizacion cors para que sirva la pagina

--- transporte_config/settings.py
+++ transporte_config/settings.py
@@ -195,12 +195,14 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'https://herrera-transporte-api.desarrollo-software.xyz',
+    'https://public-transp.desarrollo-software.xyz',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
 # CSRF
 CSRF_TRUSTED_ORIGINS = [
     'https://herrera-transporte-api.desarrollo-software.xyz',
+    'https://public-transp.desarrollo-software.xyz',
 ]
 
 # Spectacular Settings (API Documentation)
